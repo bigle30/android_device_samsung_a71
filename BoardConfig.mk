de7990174148ee92bf3b212e27af564cd226b049
Bruno Martins <bgcngm@gmail.com>
2024-04-19 22:24:21 +0200

    a71: Use the common device compatibility matrix from QCOM
    Change-Id: Ibf7bfb65d2aed182eb0ab9032840a3f17a4f376d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -138,7 +138,7 @@ DEVICE_MANIFEST_FILE := \
     $(DEVICE_PATH)/manifest.xml
 
 DEVICE_MATRIX_FILE := \
-    $(DEVICE_PATH)/compatibility_matrix.xml
+    hardware/qcom-caf/common/compatibility_matrix.xml
 
 # HWUI
 HWUI_COMPILE_FOR_PERF := true
